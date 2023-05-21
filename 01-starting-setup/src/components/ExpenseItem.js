fbf0e910e2e5320bae15f73f00494e9c40a68d3b
sainadh-paruchuri <saiparuchuri121@gmail.com>
2023-05-21 15:30:25 +0530

    starting project

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <h2>Expense Item!</h2>
+      <h3>
+        Food Rs <span>10</span>
+      </h3>
+      <h3>
+        Petrol Rs <span>100</span>
+      </h3>
+      <h3>
+        Movies Rs <span>200</span>
+      </h3>
+    </div>
+  );
+};
+
+export default ExpenseItem;
